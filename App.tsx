e5f83a1c6f53feccdcf8c10ef55c861a32b4b6d4
Chiwon <chiwon99881@gmail.com>
2019-08-02 11:18:39 +0900

    Apollo Client Setting without apollo-boost

--- App.tsx
+++ App.tsx
@@ -1,19 +1,100 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Operation, split, concat } from "apollo-link";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { WebSocketLink } from "apollo-link-ws";
+import { persistCache } from "apollo-cache-persist";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View, AsyncStorage } from "react-native";
+import { getMainDefinition } from "apollo-utilities";
+import { ApolloProvider } from "react-apollo-hooks";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [clientReady, setClientReady] = useState<any>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<any>(null);
+  const preLoad = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+    await Asset.loadAsync([require("./assets/noPhoto.jpg")]);
+    const cache = new InMemoryCache();
+    await persistCache({
+      cache,
+      storage: AsyncStorage
+    });
+    const token = await AsyncStorage.getItem("jwt");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const authMiddleware = new ApolloLink(
+      (operation: Operation, forward: any) => {
+        operation.setContext({
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        return forward(operation);
+      }
+    );
+    const httpLink = new HttpLink({
+      uri: "http://192.168.219.103:4000/graphql"
+    });
+    const wsLink = new WebSocketLink({
+      options: {
+        reconnect: true
+      },
+      uri: "ws://192.168.219.103:4000/graphql"
+    });
+    const combinedLinks = split(
+      ({ query }) => {
+        const { kind, operation }: any = getMainDefinition(query);
+        return kind === "OperationDefinition" && operation === "subscription";
+      },
+      wsLink,
+      httpLink
+    );
+    const errorLink = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    });
+    const client = new ApolloClient({
+      link: ApolloLink.from([errorLink, concat(authMiddleware, combinedLinks)]),
+      cache
+    });
+    const isLoggedInCheck = await AsyncStorage.getItem("isLoggedIn");
+    if (isLoggedInCheck === null || isLoggedInCheck === "false") {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+    setClientReady(client);
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  if (loaded && clientReady && isLoggedIn !== null) {
+    return (
+      <ApolloProvider client={clientReady}>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>GOGO</Text>
+        </View>
+      </ApolloProvider>
+    );
+  } else {
+    return <AppLoading />;
+  }
+}
---
+++ Theme.ts
@@ -0,0 +1,8 @@
+export default {
+  yellowColor: "#FFDC00",
+  brownColor: "#3B1E1E",
+  blackColor: "#262626",
+  whiteColor: "#FFFFFF",
+  boxBorder: "1px solid #e6e6e6",
+  borderColor: "#e6e6e6"
+};
